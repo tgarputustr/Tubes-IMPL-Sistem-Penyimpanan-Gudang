b03bdc42610f65beb7e12151390f926379742b15
RachmawatiAde <rachasmuch@gmail.com>
2024-12-19 08:17:19 +0700

    Update dbsistempenyimpananbarang.sql

--- dbsistempenyimpananbarang.sql
+++ dbsistempenyimpananbarang.sql
@@ -11,199 +11,146 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `dbsistempenyimpananbarang`
+-- Database: dbsistempenyimpananbarang
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
+-- Table structure for table admin
 --
 
-CREATE TABLE `admin` (
-  `id_admin` int(6) NOT NULL,
-  `nama` varchar(30) NOT NULL,
-  `email` varchar(20) NOT NULL,
-  `no_telepon` int(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
-
-
---
--- Table structure for table `customer`
---
-
-CREATE TABLE `customer` (
-  `id_customer` int(6) NOT NULL,
-  `nama` varchar(30) NOT NULL,
-  `email` varchar(20) NOT NULL,
-  `perusahaan` varchar(20) NOT NULL,
-  `no_telepon` int(15) NOT NULL
+CREATE TABLE admin (
+  id_admin int(6) NOT NULL AUTO_INCREMENT,
+  nama varchar(30) NOT NULL,
+  email varchar(50) NOT NULL,
+  no_telepon varchar(15) NOT NULL,
+  PRIMARY KEY (id_admin),
+  UNIQUE KEY email (email)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pembayaran`
+-- Table structure for table customer
 --
 
-CREATE TABLE `pembayaran` (
-  `id_pembayaran` int(6) NOT NULL,
-  `metode_bayar` varchar(30) NOT NULL,
-  `jadwal_pembayaran` date NOT NULL,
-  `jumlah` decimal(10,0) NOT NULL,
-  `id_customer` int(6) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- ------------------------------------------------------
--- Table structure for table `barang`
---
-
-CREATE TABLE `barang` (
-  `id_barang` int(5) NOT NULL,
-  `nama_barang` varchar(30) NOT NULL,
-  `jenis_barang` varchar(30) NOT NULL,
-  `berat` int(10) NOT NULL,
-  `kapasitas` varchar(30) NOT NULL,
-  `status` varchar(30) NOT NULL,
-  `id_gudang` int(5) NOT NULL
+CREATE TABLE customer (
+  id_customer int(6) NOT NULL AUTO_INCREMENT,
+  nama varchar(30) NOT NULL,
+  email varchar(50) NOT NULL,
+  perusahaan varchar(50) NOT NULL,
+  no_telepon varchar(15) NOT NULL,
+  PRIMARY KEY (id_customer),
+  UNIQUE KEY email (email)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `furniture`
+-- Table structure for table pembayaran
 --
 
-CREATE TABLE `furniture` (
-  `id_barang` int(5) NOT NULL,
-  `bahan` varchar(30) NOT NULL,
-  `dimensi` varchar(30) NOT NULL
+CREATE TABLE pembayaran (
+  id_pembayaran int(6) NOT NULL AUTO_INCREMENT,
+  metode_bayar varchar(30) NOT NULL,
+  jadwal_pembayaran date NOT NULL,
+  jumlah decimal(10,0) NOT NULL,
+  id_customer int(6) NOT NULL,
+  PRIMARY KEY (id_pembayaran),
+  KEY id_customer (id_customer),
+  FOREIGN KEY (id_customer) REFERENCES customer (id_customer)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+-- Table structure for table barang
+--
+
+CREATE TABLE barang (
+  id_barang int(5) NOT NULL AUTO_INCREMENT,
+  nama_barang varchar(30) NOT NULL,
+  jenis_barang varchar(30) NOT NULL,
+  berat int(10) NOT NULL,
+  kapasitas varchar(30) NOT NULL,
+  status varchar(30) NOT NULL,
+  id_gudang int(5) NOT NULL,
+  PRIMARY KEY (id_barang),
+  KEY id_gudang (id_gudang),
+  FOREIGN KEY (id_gudang) REFERENCES gudang (id_gudang)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `gudang`
+-- Table structure for table furniture
 --
 
-CREATE TABLE `gudang` (
-  `id_gudang` int(5) NOT NULL,
-  `jenis_gudang` varchar(30) NOT NULL,
-  `kapasitas` varchar(30) NOT NULL,
-  `lokasi` varchar(30) NOT NULL,
-  `status` varchar(30) NOT NULL
->>>>>>> modul/input_barang
+CREATE TABLE furniture (
+  id_barang int(5) NOT NULL,
+  bahan varchar(30) NOT NULL,
+  dimensi varchar(30) NOT NULL,
+  PRIMARY KEY (id_barang),
+  FOREIGN KEY (id_barang) REFERENCES barang (id_barang)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
 --
+-- Table structure for table gudang
+--
+
+CREATE TABLE gudang (
+  id_gudang int(5) NOT NULL AUTO_INCREMENT,
+  jenis_gudang varchar(30) NOT NULL,
+  kapasitas varchar(30) NOT NULL,
+  lokasi varchar(30) NOT NULL,
+  status varchar(30) NOT NULL,
+  PRIMARY KEY (id_gudang)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id_admin`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- Indexes for table `customer`
---
-ALTER TABLE `customer`
-  ADD PRIMARY KEY (`id_customer`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- Indexes for table `pembayaran`
---
-ALTER TABLE `pembayaran`
-  ADD PRIMARY KEY (`id_pembayaran`),
-  ADD KEY `id_customer` (`id_customer`);
-
---
--- Indexes for table `barang`
---
-ALTER TABLE `barang`
-  ADD PRIMARY KEY (`id_barang`),
-  ADD KEY `id_gudang` (`id_gudang`);
-
-  --
--- Indexes for table `food`
---
-ALTER TABLE `food`
-  ADD KEY `id_barang` (`id_barang`);
-
---
--- Indexes for table `furniture`
---
-ALTER TABLE `furniture`
-  ADD KEY `id_barang` (`id_barang`);
-
---
--- Indexes for table `gudang`
---
-ALTER TABLE `gudang`
-  ADD PRIMARY KEY (`id_gudang`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `admin`
+-- AUTO_INCREMENT for table admin
 --
-ALTER TABLE `admin`
-  MODIFY `id_admin` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2024001;
+ALTER TABLE admin
+  MODIFY id_admin int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2024001;
 
 --
--- AUTO_INCREMENT for table `customer`
+-- AUTO_INCREMENT for table customer
 --
-ALTER TABLE `customer`
-  MODIFY `id_customer` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24001;
+ALTER TABLE customer
+  MODIFY id_customer int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24001;
 
 --
--- AUTO_INCREMENT for table `pembayaran`
+-- AUTO_INCREMENT for table pembayaran
 --
-ALTER TABLE `pembayaran`
-  MODIFY `id_pembayaran` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25001;
+ALTER TABLE pembayaran
+  MODIFY id_pembayaran int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25001;
 
 --
--- Constraints for dumped tables
+-- AUTO_INCREMENT for table barang
 --
+ALTER TABLE barang
+  MODIFY id_barang int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10001;
 
 --
--- Constraints for table `pembayaran`
+-- AUTO_INCREMENT for table gudang
 --
-ALTER TABLE `pembayaran`
-  ADD CONSTRAINT `pembayaran_ibfk_1` FOREIGN KEY (`id_customer`) REFERENCES `customer` (`id_customer`);
+ALTER TABLE gudang
+  MODIFY id_gudang int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5001;
 
--- Constraints for table `barang`
---
-ALTER TABLE `barang`
-  ADD CONSTRAINT `barang_ibfk_1` FOREIGN KEY (`id_gudang`) REFERENCES `gudang` (`id_gudang`);
-
---
--- Constraints for table `food`
---
-ALTER TABLE `food`
-  ADD CONSTRAINT `food_ibfk_1` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id_barang`);
-
---
--- Constraints for table `furniture`
---
-ALTER TABLE `furniture`
-  ADD CONSTRAINT `furniture_ibfk_1` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id_barang`);
-  
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
